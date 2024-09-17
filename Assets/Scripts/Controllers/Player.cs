f4c05d2d8c19d8a464e297a74218c727f4fad1c1
SHERNET\macleocu <curtis11macleod@gmail.com>
2024-09-17 14:34:15 -0400

    Tasks 1B and 1C: acceleration and deceleration

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,33 +10,50 @@ public class Player : MonoBehaviour
 	public GameObject bombPrefab;
 	public Transform bombsTransform;
 
-	public float moveSpeed;
+	public float moveSpeed, accelerationTime, decelerationTime;
 	Vector3 moveVelocity;
 
 	void Update()
 	{
-		PlayerMovement();
+		PlayerMovement(); // Invoke every frame
 	}
 
-	void PlayerMovement()
+	float Clamp(float change)
 	{
-		if (Input.GetKey(KeyCode.UpArrow)) moveVelocity.y = 1;
-		else if (Input.GetKey(KeyCode.DownArrow)) moveVelocity.y = -1;
-		else moveVelocity.y = 0;
+		return Mathf.Clamp(change, -1, 1);
+	}
 
-		if (Input.GetKey(KeyCode.LeftArrow)) moveVelocity.x = -1;
-		else if (Input.GetKey(KeyCode.RightArrow)) moveVelocity.x = 1;
-		else moveVelocity.x = 0;
+	float Decelerate(float currentValue)
+	{
+		// If the value is below 0, just be instant. Otherwise divide the distance by the time (1 unit, 3000 milliseconds for example)
+		float decelerationSpeed = (decelerationTime > 0) ? (1.0f / (decelerationTime * 1000)) : 1.0f;
 
-		// Keep in screen
-		Vector3 moveVector = (moveSpeed * Time.deltaTime * moveVelocity);
-		Vector3 intendedPosOnScreen = Camera.main.WorldToViewportPoint(transform.position + moveVector);
+		// Add up to 0 if negative, subtract down to 0 if positive
+		return (currentValue < 0) ? Mathf.Clamp(currentValue + decelerationSpeed, -1, 0) : Mathf.Clamp(currentValue - decelerationSpeed, 0, 1);
+	}
 
-		if (intendedPosOnScreen.x < 0.0 || 1.0 < intendedPosOnScreen.x) moveVector.x = 0;
-		if (intendedPosOnScreen.y < 0.0 || 1.0 < intendedPosOnScreen.y) moveVector.y = 0;
+	public void PlayerMovement()
+	{
+		// Re-calculate the speed in case the accelerationTime value changes during runtime
+		float accelerationSpeed = (accelerationTime > 0) ? (1.0f / (accelerationTime * 1000)) : 1.0f; // Make sure the value is above 0
+
+		// Up and down arrows cannot be held at the same time; prioritize one or the other
+		if (Input.GetKey(KeyCode.UpArrow)) moveVelocity.y = Clamp(moveVelocity.y + accelerationSpeed); // (?, 1)
+		else if (Input.GetKey(KeyCode.DownArrow)) moveVelocity.y = Clamp(moveVelocity.y - accelerationSpeed); // (?, -1)
+		else moveVelocity.y = Decelerate(moveVelocity.y); // (?, 0) (neither being held down)
+
+		if (Input.GetKey(KeyCode.LeftArrow)) moveVelocity.x = Clamp(moveVelocity.x - accelerationSpeed); // (-1, ?)
+		else if (Input.GetKey(KeyCode.RightArrow)) moveVelocity.x = Clamp(moveVelocity.x + accelerationSpeed); // (1, ?)
+		else moveVelocity.x = Decelerate(moveVelocity.x); // (0, ?)
+
+		// Keep on the screen
+		Vector3 moveVector = (moveSpeed * Time.deltaTime * moveVelocity); // Where we want the position to be
+		Vector3 intendedPosOnScreen = Camera.main.WorldToViewportPoint(transform.position + moveVector); // Check if this position is within the viewport
+
+		if (intendedPosOnScreen.x < 0.0 || intendedPosOnScreen.x > 1.0) moveVector.x = 0; // This position is to the left or right of the viewport; freeze X
+		if (intendedPosOnScreen.y < 0.0 || intendedPosOnScreen.y > 1.0) moveVector.y = 0; // Same but for Y. Overwrites moveVector
 
 		// Move
-		transform.position += moveVector;
-
+		transform.position += moveVector; // Add the new filtered move vector to the position
 	}
 }
